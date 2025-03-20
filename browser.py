cb7294af55ae6270f410903328c5660dbe38bd8c
wxt <3264117476@qq.com>
2025-03-20 20:54:45 +0800

    feat: improve the speed via javascript

--- browser.py
+++ browser.py
@@ -2,19 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-import selenium
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--allow-running-insecure-content')
 chrome_options.add_argument("blink-settings=imagesEnabled=false")
 chrome_options.add_argument("--user-data-dir=/home/wxt/.chrome-generals")
+chrome_options.set_capability('pageLoadStrategy', 'eager')
 
 driver = webdriver.Chrome(
         service=Service('./result/bin/chromedriver'),
@@ -22,8 +21,7 @@ driver = webdriver.Chrome(
 
 driver.get("https://generals.io")
 driver.get("https://generals.io/replays/eOzEsKuZd?p=wxt1221")
-
-time.sleep(1)
+print("Get the Page!")
 
 next_step = driver.find_element(
         By.XPATH,
@@ -32,46 +30,44 @@ next_step = driver.find_element(
 mapall = []
 endall = 1
 ii = ""
+cnt = 0
+element_all = driver.find_element(
+                    By.XPATH,
+                    "/html/body/div/div/div/div[2]/table/tbody")
+element_all = element_all.find_elements(
+                    By.XPATH,
+                    "./*")
+turn = driver.find_element(By.XPATH, "//*[@id=\"turn-counter\"]")
+map = []
+for iii in element_all:
+    maptemp = []
+    element_hang = iii.find_elements(
+                    By.XPATH,
+                    "./*")
+    map.append(element_hang)
+i = len(map)
+j = len(map[0])
 while endall:
-    i = 1
-    j = 0
-    end1 = 1
-    map = []
-    while end1:
-        curj = 0
-        end2 = 1
-        maptemp = []
-        try:
-            element = driver.find_element(
-                    By.CSS_SELECTOR,
-                    f"#gameMap > tbody > tr:nth-child({i})")
-        except selenium.common.exceptions.NoSuchElementException:
-            end1 = 0
-        else:
-            while end2:
-                curj = curj + 1
-                try:
-                    element = driver.find_element(
-                            By.CSS_SELECTOR,
-                            f"#gameMap > tbody > tr:nth-child({i}) > td:nth-child({curj})")  # noqa: E501
-                except selenium.common.exceptions.NoSuchElementException:
-                    end2 = 0
-                else:
-                    maptemp.append(
-                            [element.get_attribute("class"),
-                             element.text])
-            map.append(maptemp)
-            j = curj
-            i = i + 1
-    i = i - 1
-    j = j - 1
+    mapcur = (driver.execute_script("""
+    let list = [];
+    for(var i=0;i<arguments[0].length;i++){
+        let list2 = [];
+        for(var j=0;j<arguments[0][i].length;j++){
+            let list3 = [];
+            list3.push(arguments[0][i][j].innerText);
+            list3.push(arguments[0][i][j].className);
+            list2.push(list3);
+        }
+        list.push(list2)
+    }
+    return list;""", map))
+    begin_time = time.time()
     next_step.click()
-    element = driver.find_element(By.XPATH, "//*[@id=\"turn-counter\"]")
-    if ii == element.text:
+    end_time = time.time()
+    print(end_time-begin_time)
+    if ii == turn.text:
         endall = 0
-    else:
-        ii = element.text
-        print("Down "+ii)
-        mapall.append(map)
-        print(map)
+    ii = turn.text
+    print("Down "+ii)
+    mapall.append(mapcur)
 driver.quit()
